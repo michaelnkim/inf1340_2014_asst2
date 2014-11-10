ca5edfcd7e5e443dbafe77c4faea5a45443452e2
michaelnkim <michaeln.kim@mail.utoronto.ca>
2014-11-10 16:25:47 -0500

    Working????

--- papers.py
+++ papers.py
@@ -5,7 +5,7 @@
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 
-__copyright__ = "2014 Susan Sim"
+__copyright__ = "2014 Susan Si"
 __license__ = "MIT License"
 
 __status__ = "Prototype"
