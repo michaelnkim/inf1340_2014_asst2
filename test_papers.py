1bf187c28b0dc84b071773af6a9e931d1aab8447
michaelnkim <michaeln.kim@mail.utoronto.ca>
2014-11-10 18:04:19 -0500

    Working!

--- test_papers.py
+++ test_papers.py
@@ -23,6 +23,17 @@ def test_basic():
     assert decide("test_quarantine.json", "watchlist.json", "countries.json") == ["Quarantine"]
     assert decide("test_reject0.json", "watchlist.json", "countries.json") == ["Reject"]
     #Doesn't have Last name, should be rejected
+#    assert decide("test_invalid_visa.json", "watchlist.json", "countries.json") == ["Accept"]
+    #Expired Visa of 1999
+
+    #Tested 4 cases just containing all 3 conditions to test whether priority is set.
+    assert decide("test_Priority_Quarantine.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    #Via Country ELE = Quarantine
+    assert decide("test_Priority_Reject.json", "watchlist.json", "countries.json") == ["Reject"]
+    #Doesn't have Visa field
+    assert decide("test_Priority_Secondary.json", "watchlist.json", "countries.json") == ["Accept"]
+
+    assert decide("test_Priority_Accept.json", "watchlist.json", "countries.json") == ["Accept"]
 
 
 def test_files():
